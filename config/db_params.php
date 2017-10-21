0541448a777e07a5b946096edf431b8142672041
Victor Gordeev <gnexus460@gmail.com>
2017-10-21 17:22:10 +0300

    Add project

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'host' => 'db3.ipipe.ru',
+	'dbname' => 'vera_db58',
+	'user' => 'vera_db58',
+	'password' => 'GyrvYAPV7CbU',
+);
---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+	'news/([0-9]+)' => 'news/view/$1',
+	'news' => 'news/index',
+
+	//'news' => 'news/index',	// action Index в NewsController
+	//'news/([0-9]+)' => 'news/view',
+	//'products' => 'product/list', // action List в ProductController
+// ProductController
+	//'news/archive' => 'news/archive',
+	);
